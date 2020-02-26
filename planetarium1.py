9a582ed6e524de1302c882c8fd0ff74990685942
trumangraham <trumangraham@amdg.rockhursths.edu>
2020-02-26 08:04:25 -0600

    plaetarium

---
+++ planetarium1.py
@@ -0,0 +1,12 @@
+class planet(object):
+
+    def __init__(self, planet, description, tempature):
+        self.planet = planet
+        self.description = description
+        self.tempature = tempature
+
+    def __str__(self):
+        return f"Planet: {self.planet}"
+        return f"Description: {description}"
+        return f"Tempature: {tempature}"
+    
