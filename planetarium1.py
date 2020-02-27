57cac6f81edc3eaae0ee1d31c7a0759b50bbcde5
trumangraham <trumangraham@amdg.rockhursths.edu>
2020-02-27 09:47:57 -0600

    planetarium2

--- planetarium1.py
+++ planetarium1.py
@@ -1,12 +1,11 @@
 class planet(object):
 
-    def __init__(self, planet, description, tempature):
+    def __init__(self, planet, description,moons, tempature):
         self.planet = planet
         self.description = description
+        self.moons = moons
         self.tempature = tempature
 
     def __str__(self):
-        return f"Planet: {self.planet}"
-        return f"Description: {description}"
-        return f"Tempature: {tempature}"
+        return f"Planet: {self.planet} \nDescription: {self.description} \nMoons: {self.moons} \nAverage Tempature: {self.tempature} degrees"
     
