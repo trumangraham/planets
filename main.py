faea48e454ce899624540ad217dd921b7bc07a77
trumangraham <trumangraham@amdg.rockhursths.edu>
2020-02-27 09:46:50 -0600

    main

---
+++ main.py
@@ -0,0 +1,20 @@
+from planetarium1 import planet
+
+
+mercury = planet("Mercury", "hot", 1, 330)
+venus = planet("Venus", "small", 2, 132)
+earth = planet("Earth", "habitable", 1, 76)
+mars = planet("Mars", "red", 2, 53)
+jupiter = planet("Jupiter", "big", 12, -19)
+saturn = planet("Saturn", "ringy", 4, -78)
+uranus = planet("Uranus", "gasy", 3, -219)
+neptune = planet("Neptune", "blue", 7, -347)
+
+print(mercury)
+print(venus)
+print(earth)
+print(mars)
+print(jupiter)
+print(saturn)
+print(uranus)
+print(neptune)
