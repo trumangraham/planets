409e77901cd3ff3c45664d6193d19f94d1e4864b
trumangraham <trumangraham@amdg.rockhursths.edu>
2020-02-28 08:44:22 -0600

    main

--- main.py
+++ main.py
@@ -10,25 +10,40 @@ uranus = planet("Uranus", "gasy", 3, -219)
 neptune = planet("Neptune", "blue", 7, -347)
 
 
-print("What planet would you like to see?")
-p = input()
+p = input("Would you like to see some planets?")
 
-if(p == "mercury"):
-    print(mercury)
-if(p == "venus"):
-    print(venus)
-if(p == "earth"):
-    print(earth)
-if(p == "mars"):
-    print(mars)
-if(p == "jupiter"):
-    print(jupiter)
-if(p == "saturn"):
-    print(saturn)
-if(p == "uranus"):
-    print(uranus)
-if(p == "neptune"):
-    print(neptune)
+while (p != 'no'):
+    print()
+    print("1: Mercury")
+    print("2: Venus")
+    print("3: Earth")
+    print("4: Mars")
+    print("5: Jupiter")
+    print("6: Saturn")
+    print("7: Uranus")
+    print("8: Neptune")
+    
+    print()
+    choice = input("Which one would you like to see?")
+    print()
+    if(choice == "1"):
+        print(mercury)
+    if(choice == "2"):
+        print(venus)
+    if(choice == "3"):
+        print(earth)
+    if(choice == "4"):
+        print(mars)
+    if(choice == "5"):
+        print(jupiter)
+    if(choice == "6"):
+        print(saturn)
+    if(choice == "7"):
+        print(uranus)
+    if(choice == "8"):
+        print(neptune)
+
+
     
 
     
