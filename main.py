95abdc686b039ad1004a0a2d7936ab66bbcbe8b7
trumangraham <trumangraham@amdg.rockhursths.edu>
2020-02-28 08:34:53 -0600

    main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from planetarium1 import planet
 
-
 mercury = planet("Mercury", "hot", 1, 330)
 venus = planet("Venus", "small", 2, 132)
 earth = planet("Earth", "habitable", 1, 76)
@@ -10,11 +9,30 @@ saturn = planet("Saturn", "ringy", 4, -78)
 uranus = planet("Uranus", "gasy", 3, -219)
 neptune = planet("Neptune", "blue", 7, -347)
 
-print(mercury)
-print(venus)
-print(earth)
-print(mars)
-print(jupiter)
-print(saturn)
-print(uranus)
-print(neptune)
+
+print("What planet would you like to see?")
+p = input()
+
+if(p == "mercury"):
+    print(mercury)
+if(p == "venus"):
+    print(venus)
+if(p == "earth"):
+    print(earth)
+if(p == "mars"):
+    print(mars)
+if(p == "jupiter"):
+    print(jupiter)
+if(p == "saturn"):
+    print(saturn)
+if(p == "uranus"):
+    print(uranus)
+if(p == "neptune"):
+    print(neptune)
+    
+
+    
+    
+
+
+
